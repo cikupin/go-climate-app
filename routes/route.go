8c52a5547cd00c8f28e1b3b711123735ecb7c01c
Alvin Rizki <alvin.rizki@kudo.co.id>
2018-06-08 15:08:07 +0700

    add public content

--- routes/route.go
+++ routes/route.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/cikupin/go-climate-app/controllers"
 	"github.com/cikupin/go-climate-app/interfaces"
 	"github.com/gorilla/mux"
@@ -17,7 +19,10 @@ func (r *Router) Handler() *mux.Router {
 	r.IndexController = new(controllers.IndexController)
 	r.AjaxController = new(controllers.AjaxController)
 
-	routes := mux.NewRouter().StrictSlash(false)
+	fs := http.FileServer(http.Dir("public"))
+	routes := mux.NewRouter()
+
+	routes.PathPrefix("/public/").Handler(http.StripPrefix("/public/", fs))
 	routes.HandleFunc("/", r.IndexController.Index).Methods("GET")
 	routes.HandleFunc("/get-weather/{city}/{days}", r.AjaxController.GetWeather).Methods("GET")
 
